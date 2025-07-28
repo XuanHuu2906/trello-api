2d5abdcf8767d0b97bfbf24f85edcc374cd61f85
HuuNguyen <huukongu@gmail.com>
2025-07-28 11:08:03 +0700

    connect_monngodb_env_variables

---
+++ src/models/boardModel.js
@@ -0,0 +1,58 @@
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+
+import Joi from 'joi'
+import { ObjectId } from 'mongodb'
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
+import { GET_DB } from '~/config/mongodb'
+
+const BOARD_COLLECTION_NAME = 'Boards'
+const BOARD_COLLECTION_SCHEMA = Joi.object({
+  title: Joi.string().required().min(3).max(50).trim().strict(),
+  slug: Joi.string().required().min(3).trim().strict(),
+  description: Joi.string().required().min(3).max(256).trim().strict(),
+
+  columnOrderIds: Joi.array().items(
+    Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
+  ).default([]),
+
+  createdAT: Joi.date().timestamp('javascript').default(Date.now),
+  updatedAT: Joi.date().timestamp('javascript').default(null),
+  _destroy: Joi.boolean().default(false)
+})
+
+const validateBeforeCreate = async (data) => {
+  return await BOARD_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false })
+}
+
+const createNew = async (data) => {
+  try {
+    const validData = await validateBeforeCreate(data)
+
+    const createBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(validData)
+    return createBoard
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const findOnebyId = async (id) => {
+  try {
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
+      _id: new ObjectId(toString(id))
+    })
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+export const boardModel = {
+  BOARD_COLLECTION_NAME,
+  BOARD_COLLECTION_SCHEMA,
+  createNew,
+  findOnebyId
+}
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,3 +3,17 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRoute } from '~/routes/v1/boardRoute'
+
+const Route = express.Router()
+
+Route.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({ message: 'APIs V1 are ready to use.'})
+})
+
+Route.use('/boards', boardRoute)
+
+export const APIs_V1 = Route
